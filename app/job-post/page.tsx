9121b4d5b8f26015f2fc12c2226ef84aaffd00ad
Akmal Raafid Taufiqurrahman <akmalraafid25@gmail.com>
2025-09-05 16:12:03 +0700

    Update page.tsx

--- app/job-post/page.tsx
+++ app/job-post/page.tsx
@@ -9,7 +9,7 @@ import { createChat } from '@n8n/chat';
 export default function ChatbotPage() {
   useEffect(() => {
 		createChat({
-			webhookUrl: 'https://hrautomationsupport.app.n8n.cloud/webhook/4cbd001a-f192-4867-b9e9-79f3742b7420/chat',
+			webhookUrl: 'https://hr-automationsone.app.n8n.cloud/webhook/4cbd001a-f192-4867-b9e9-79f3742b7420/chat',
       initialMessages:['👋 Hello! I’m Hira', 'I’m here to help you craft clear, professional, and engaging job postings that attract the right candidates. Just tell me the role you’re hiring for, along with any key details (skills, experience, location, etc.), and I’ll generate a tailored job post for you'
       ],
 		});
