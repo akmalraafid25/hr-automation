93368f44b710ec4e97064d62e88375e4f9fb8acb
Akmal Raafid Taufiqurrahman <akmalraafid25@gmail.com>
2025-09-09 11:30:01 +0700

    Update 09-09-2025 11:29
    Update calendar visual

--- app/application/page.tsx
+++ app/application/page.tsx
@@ -10,10 +10,8 @@ export default function DashboardPage() {
   return (
     <div className="flex h-screen bg-background">
       <DashboardSidebar />
-
       <div className="flex-1 flex flex-col overflow-hidden">
         <DashboardHeader />
-
         <main className="flex-1 overflow-y-auto">
           <div className="container mx-auto p-6 space-y-6">
             <div>
--- components/calendar.tsx
+++ components/calendar.tsx
@@ -342,24 +342,24 @@ export default function CalendarPage() {
                                 </span>
                               )}
                             </div>
-                            <Dialog>
-                                    <div className="py-2">
-                                      <Button>
-                                        <DialogTrigger>
-                                          Details
-                                        </DialogTrigger>
-                                      </Button>
-                                    </div>
-                                    <DialogContent>
-                                        <DialogHeader>
-                                            <DialogTitle>Are you absolutely sure?</DialogTitle>
-                                            <DialogDescription>
-                                                This action cannot be undone. This will permanently delete your account
-                                                and remove your data from our servers.
-                                            </DialogDescription>
-                                        </DialogHeader>
-                                    </DialogContent>
-                            </Dialog>
+                            <div className="py-2">
+                              <Dialog>
+                                <DialogTrigger asChild>
+                                  <Button size="sm">
+                                    Details
+                                  </Button>
+                                </DialogTrigger>
+                                <DialogContent>
+                                    <DialogHeader>
+                                        <DialogTitle>Are you absolutely sure?</DialogTitle>
+                                        <DialogDescription>
+                                            This action cannot be undone. This will permanently delete your account
+                                            and remove your data from our servers.
+                                        </DialogDescription>
+                                    </DialogHeader>
+                                </DialogContent>
+                              </Dialog>
+                            </div>
                           </div>
                           <DropdownMenu>
                             <DropdownMenuTrigger asChild>
