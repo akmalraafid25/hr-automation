0482b1bbfaf961a10dc2526964c55af904810ded
Akmal Raafid Taufiqurrahman <akmalraafid25@gmail.com>
2025-09-16 01:35:36 +0700

    Update login page design

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -72,11 +72,11 @@ export function LoginForm({ className, ...props }: React.ComponentProps<"div">) 
             </div>
           </form>
 
-          <div className="bg-muted relative hidden md:block">
+          <div className="relative hidden md:block">
             <img
-              src="https://www.softwareone.com/_next/image?url=%2F-%2Fmedia%2Fimages%2Fgeneral%2Ftime-lapse-of-lights-unsplash-3shfnfzdfvc-square.jpg%3Fh%3D600%26iar%3D0%26w%3D600%26hash%3D8F2465023BE1F246C9FB8A1EC8765E20&w=1920&q=90"
+              src="https://www.softwareone.com/_next/image?url=%2F-%2Fmedia%2Fimages%2Ffuture-forward%2Fbubbles-getty-1367572322-teaser.jpg%3Fh%3D405%26iar%3D0%26w%3D748%26hash%3D5063C47C08FC263B7F200611E196042E&w=992&q=90"
               alt="Image"
-              className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+              className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
             />
           </div>
         </CardContent>
