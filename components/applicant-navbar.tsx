618089ff3fd8f8a862511a9b4d9a1d19fde0a256
Akmal Raafid Taufiqurrahman <akmalraafid25@gmail.com>
2025-09-18 22:05:40 +0700

    Update

--- components/applicant-navbar.tsx
+++ components/applicant-navbar.tsx
@@ -19,7 +19,7 @@ export function ApplicantNavbar() {
           <div className="flex items-center space-x-8">
             <a href="/jobs">
               <Image
-                src="https://www.softwareone.com/-/media/images/logos/softwareone-logo-blk.svg?iar=0&hash=6A277FF39328B4D79A071F4A9F95F301"
+                src="/logo.svg"
                 width={100}
                 height={40}
                 alt="Company Logo"
